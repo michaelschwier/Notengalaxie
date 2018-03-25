eb4688c98ab3dcaf040a414cdfe8ef1b781f4d23
Michael Schwier <mschwier@bwh.harvard.edu>
2018-03-25 15:44:16 -0400

    More test level definitions

--- scripts/levelDefinitions.js
+++ scripts/levelDefinitions.js
@@ -1,42 +1,100 @@
 levelDefinitions = [
   {
     name: "Level 1",
+    minSpawnTime: 4.0,
     planets: [
       {
         imageKey: "c1",
-        buttonKey: "bc",
         audioKey: "c"
       },
       {
-        imageKey: "e",
-        buttonKey: "be",
-        audioKey: "e" 
-      },
-      {
         imageKey: "g",
-        buttonKey: "bg",
         audioKey: "g"
       }
     ]
   },
   {
     name: "Level 2",
+    minSpawnTime: 4.0,
+    planets: [
+      {
+        imageKey: "c1",
+        audioKey: "c"
+      },
+      {
+        imageKey: "e",
+        audioKey: "e" 
+      },
+      {
+        imageKey: "g",
+        audioKey: "g"
+      }
+    ]
+  },
+  {
+    name: "Level 3",
+    minSpawnTime: 4.0,
     planets: [
       {
         imageKey: "g",
-        buttonKey: "bg",
         audioKey: "g"
       },
       {
         imageKey: "e",
-        buttonKey: "be",
         audioKey: "e" 
       },
       {
         imageKey: "c1",
-        buttonKey: "bc",
         audioKey: "c"
       }
     ]
-  }
+  },
+  {
+    name: "Level 4",
+    minSpawnTime: 4.0,
+    planets: [
+      {
+        imageKey: "c1",
+        audioKey: "c"
+      },
+      {
+        imageKey: "g",
+        audioKey: "g"
+      },
+      {
+        imageKey: "e",
+        audioKey: "e" 
+      },
+      {
+        imageKey: "c1",
+        audioKey: "c"
+      }
+    ]
+  },
+  {
+    name: "Level 5",
+    minSpawnTime: 4.0,
+    planets: [
+      {
+        imageKey: "c1",
+        audioKey: "c"
+      },
+      {
+        imageKey: "g",
+        audioKey: "g"
+      },
+      {
+        imageKey: "e",
+        audioKey: "e" 
+      },
+      {
+        imageKey: "c1",
+        audioKey: "c"
+      },
+      {
+        imageKey: "e",
+        audioKey: "e" 
+      }
+    ]
+  }  
 ]
