fa7fc044adad77428cb8523b4d8e1af8654895c8
Michael Schwier <mschwier@bwh.harvard.edu>
2018-04-11 01:09:07 -0400

    more levels

--- scripts/levelDefinitions.js
+++ scripts/levelDefinitions.js
@@ -1,11 +1,25 @@
 levelDefinitions = [
   {
-    name: "Level 1",
+    name: "Oktave",
     minSpawnTime: 5.0,
     planets: [
       {
         imageKey: "c1",
-        audioKey: "c"
+        audioKey: "c1"
+      },
+      {
+        imageKey: "c2",
+        audioKey: "c2"
+      }
+    ]
+  },
+  {
+    name: "Quinte",
+    minSpawnTime: 5.0,
+    planets: [
+      {
+        imageKey: "c1",
+        audioKey: "c1"
       },
       {
         imageKey: "g",
@@ -14,12 +28,40 @@ levelDefinitions = [
     ]
   },
   {
-    name: "Level 2",
+    name: "Große Terz C",
     minSpawnTime: 5.0,
     planets: [
       {
         imageKey: "c1",
-        audioKey: "c"
+        audioKey: "c1"
+      },
+      {
+        imageKey: "e",
+        audioKey: "e"
+      }
+    ]
+  },
+  {
+    name: "Kleine Terz E",
+    minSpawnTime: 5.0,
+    planets: [
+      {
+        imageKey: "e",
+        audioKey: "e"
+      },
+      {
+        imageKey: "g",
+        audioKey: "g"
+      }
+    ]
+  },
+  {
+    name: "Dreikklang C",
+    minSpawnTime: 5.0,
+    planets: [
+      {
+        imageKey: "c1",
+        audioKey: "c1"
       },
       {
         imageKey: "e",
@@ -32,7 +74,43 @@ levelDefinitions = [
     ]
   },
   {
-    name: "Level 3",
+    name: "Dreikklang E",
+    minSpawnTime: 5.0,
+    planets: [
+      {
+        imageKey: "e",
+        audioKey: "e"
+      },
+      {
+        imageKey: "g",
+        audioKey: "g" 
+      },
+      {
+        imageKey: "h",
+        audioKey: "h"
+      }
+    ]
+  },
+  {
+    name: "Dreikklang D",
+    minSpawnTime: 5.0,
+    planets: [
+      {
+        imageKey: "d",
+        audioKey: "d"
+      },
+      {
+        imageKey: "f",
+        audioKey: "f" 
+      },
+      {
+        imageKey: "a",
+        audioKey: "a"
+      }
+    ]
+  },
+  {
+    name: "Dreiklang C Invertiert",
     minSpawnTime: 5.0,
     planets: [
       {
@@ -45,8 +123,29 @@ levelDefinitions = [
       },
       {
         imageKey: "c1",
-        audioKey: "c"
+        audioKey: "c1"
       }
     ]
+  },
+  {
+    name: "Vierklang C",
+    minSpawnTime: 5.0,
+    planets: [
+      {
+        imageKey: "c1",
+        audioKey: "c1"
+      },
+      {
+        imageKey: "e",
+        audioKey: "e" 
+      },
+      {
+        imageKey: "g",
+        audioKey: "g"
+      },
+      {
+        imageKey: "h",
+        audioKey: "h"
+      }    ]
   }
 ]
